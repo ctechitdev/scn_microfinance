a80394e0fae4d1021a26da32a610d36a65198ce7
Thaksin-soft <thaksin.kvs@gmail.com>
2023-10-11 20:22:40 +0700

    update 11-10-2023 8:22PM

--- src/controllers/function/login_controller.js
+++ src/controllers/function/login_controller.js
@@ -3,18 +3,27 @@ const request = require("express/lib/request");
 const connected = require('../../../setting/connect');
 
 const queries = require('../../queries/function/login_query');
+const { response } = require("express");
 
 const login = (request , respond) => {
 
-    
+    const { username, password } = request.body;
+
+
+    console.log(username);
+    console.log(password);
   
-  connected.query(queries.login_user, (error, results) => {
+  connected.query(queries.login_user,[username, password], (error, results) => {
     if(error) throw error;
 
-      
+    if(results.length){
+      respond.status(200).json(results);
+    }else{
+      respond.status(200).send("ຊື່ຜູ້ໃຊ້ຫຼືລະຫັດຜ່ານບໍ່ຖືກຕ້ອງ");
+    }   
   //  console.log(results);
 
-    respond.status(200).json(results);
+    
 
   });
 };
--- src/queries/function/login_query.js
+++ src/queries/function/login_query.js
@@ -1,4 +1,4 @@
-const login_user = "select * from tbl_user";
+const login_user = "select * from tbl_user where user_name= ? and user_password=?";
 
 
 module.exports = {
