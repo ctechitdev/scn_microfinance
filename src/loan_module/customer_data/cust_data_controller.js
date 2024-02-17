e723c86a2a388bf8a523247dbda0cf4f2c7bdd9e
ctechitdev <ctech.itdev@gmail.com>
2024-02-17 10:15:21 +0700

    bo back-end 20240217 10:15

--- src/loan_module/customer_data/cust_data_controller.js
+++ src/loan_module/customer_data/cust_data_controller.js
@@ -8,54 +8,57 @@ const { response } = require("express");
 
 const secretkey = "CtecMicrofinance";
 
-
 const get_customer_Data = (request, response) => {
-    const {
-        first_name,
-        last_name,
-        age,
-        phone_number,
-        whats_app_number,
-        village_namge,
-        district_id,
-        province_id,
-        picture_identified_name
-    } = request.body;
-    jwt.verify(request.token, secretkey, (token_error, rstoken) => {
-        if (token_error) {
-            response.json({ resultCode: "token error" })
-        } else {
-
-            connected.query(queries.get_data, [
-                first_name,
-                last_name,
-                age,
-                phone_number,
-                whats_app_number,
-                village_namge,
-                district_id,
-                province_id], (error, results) => {
-                    if (error) throw error;
-                    if (results.length) {
-                        response.json(results);
-
-                    } else {
-                        connected.query(queries.get_identified, [picture_identified_name], (error, results) => {
-                            if (results.length) {
-                                response.json(results);
-
-                            } else {
-                                response.json({ resultCode: "search error" })
-                            }
-                        })
-
-                    }
-                })
+  const {
+    first_name,
+    last_name,
+    age,
+    phone_number,
+    whats_app_number,
+    village_namge,
+    districts_name,
+    province_name,
+    picture_identified_name,
+  } = request.body;
+  jwt.verify(request.token, secretkey, (token_error, rstoken) => {
+    if (token_error) {
+      response.json({ resultCode: "token error" });
+    } else {
+      connected.query(
+        queries.get_data,
+        [
+          first_name,
+          last_name,
+          age,
+          phone_number,
+          whats_app_number,
+          village_namge,
+          districts_name,
+          province_name,
+        ],
+        (error, results) => {
+          if (error) throw error;
+          if (results.length) {
+            response.json(results);
+          } else {
+            connected.query(
+              queries.get_identified,
+              [picture_identified_name],
+              (error, results) => {
+                if (results.length) {
+                  response.json(results);
+                } else {
+                  response.json({ resultCode: "search error" });
+                }
+              }
+            );
+          }
         }
-    });
-}
+      );
+    }
+  });
+};
 
 module.exports = {
-
-    get_customer_Data
-}
+  get_customer_Data,
+};
--- src/loan_module/customer_data/cust_data_query.js
+++ src/loan_module/customer_data/cust_data_query.js
@@ -1,5 +1,6 @@
-const get_data = "select * from tbl_customer where first_name =? or last_name=? or age=? or phone_number=? or whats_app_number=? or village_namge=? or district_id=? or province_id =? ";
-const get_identified = "select * from tbl_picture_identified where picture_identified_name=?";
+const get_data =
+ "select first_name,last_name,age,phone_number,whats_app_number,village_namge, districts_name, province_name from tbl_customer  left join tbl_districts on tbl_customer.district_id = tbl_districts.districts_id left join tbl_province on tbl_customer.province_id = tbl_province.province_id where first_name =? or last_name=? or age=? or phone_number=? or whats_app_number=? or village_namge=? or districts_name=? or province_name =?";
+const get_identified = "select first_name,last_name,age,phone_number,whats_app_number,village_namge, districts_name, province_name,picture_identified_name from tbl_picture_identified  left join tbl_customer on tbl_picture_identified.customer_id=tbl_customer.customer_id left join tbl_districts on tbl_customer.district_id = tbl_districts.districts_id left join tbl_province on tbl_customer.province_id = tbl_province.province_id where picture_identified_name=?";
 module.exports={
     get_data,
     get_identified
