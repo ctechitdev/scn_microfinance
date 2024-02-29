3be16ac45189159c34b239fd8bfa9e83a357bb3f
ctechitdev <ctech.itdev@gmail.com>
2024-02-29 23:04:21 +0700

    bo back-end 20240229 23:04

--- src/loan_module/asset_credit_customer/asset_credit_cust_query.js
+++ src/loan_module/asset_credit_customer/asset_credit_cust_query.js
@@ -1,5 +1,5 @@
 const add_asset_credit = 
-"insert into tbl_asset_credit_customer(asset_type_customer_name, asset_detail, asset_credit_values,limit_credit_values,currency,asset_status,customer_id,add_by,add_date,evaluate_by,evaluate_date,guarantee_picture,location_point)values(?,?,?,?,?,?,?,?,current_date,?,?,?,?)"
+"insert into tbl_asset_credit_customer(asset_type_customer_name, asset_detail, asset_credit_values,limit_credit_values,currency,asset_status,customer_id,add_by,add_date,evaluate_by,evaluate_date,guarantee_picture)values(?,?,?,?,?,?,?,?,current_date,?,?,?)"
 
 
 module.exports={
